51fd248c6f6a83acce9037816e75edb3a80082ea
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2023-10-13 09:39:16 +0200

    add parameters to the cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,13 +9,55 @@ import (
 	// "github.com/bitfield/script"
 )
 
+var version = "dev"
+
+func printVersion() {
+	fmt.Printf("%s\n", version)
+}
+
 func main() {
+	var (
+		flagVersion bool
+		flagHelp    bool
+		chartName   string
+		outputDir   string
+	)
+
+	flag.StringVar(&chartName, "n", "", "Name of the chart")
+	flag.StringVar(&outputDir, "o", "", "Path of the generated chart")
+	flag.BoolVar(&flagVersion, "version", false, "Print version")
+	flag.BoolVar(&flagHelp, "help", false, "Print help")
+	flag.Parse()
+
+	if flagVersion {
+		printVersion()
+		os.Exit(0)
+	}
+
+	if flagHelp {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// TODO: check if helm is installed
+	if len(chartName) == 0 {
+		fmt.Fprintf(os.Stderr, "Error: chart name is required\n")
+		os.Exit(1)
+	}
+
+	if len(outputDir) == 0 {
+		fmt.Fprintf(os.Stderr, "Error: chart path is required\n")
+		os.Exit(1)
+	}
+
+	// TODO: check if chartPath exists (if not create it)
+
 	// p := script.Exec("gum confirm \"Are you sure?\"").WithStderr(os.Stderr).WithStdout(os.Stdout)
 	// p.Wait()
 	// fmt.Println(p.ExitStatus())
-	chartName := "myChart"
-	chartPath := "tests/tmp/myChart"
-	app := app.NewApp(chartName, chartPath, true, true, true, true)
+	// chartName := "myChart"
+	// chartPath := "tests/tmp/myChart"
+	app := app.NewApp(chartName, outputDir, true, true, true, true)
 	err := app.GenerateChart()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
